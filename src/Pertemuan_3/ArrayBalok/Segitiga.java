8ae5eb500e1cfe95883d818ddef8e695d922f7ec
mlnrengga <mlnrengga@gmail.com>
2024-02-29 20:37:52 +0700

    P3_Percobaan3

---
+++ src/Pertemuan_3/ArrayBalok/Segitiga.java
@@ -0,0 +1,19 @@
+package Pertemuan_3.ArrayBalok;
+
+public class Segitiga {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga (int a, int t){
+        alas = a;
+        tinggi = t;
+    }    
+    public double hitungLuas() {
+        return 0.5 * alas * tinggi;
+    }
+
+    public double hitungKeliling() {
+        double sisiMiring = Math.sqrt(alas * alas + tinggi * tinggi);
+        return alas + tinggi + sisiMiring;
+    }
+}
