b3011854e23ab3afce75c215c1db85a4594cf5d8
mlnrengga <mlnrengga@gmail.com>
2024-03-01 14:03:58 +0700

    P3_Praktikum2

---
+++ src/Pertemuan_3/Tugas_2/Mahasiswa.java
@@ -0,0 +1,15 @@
+package Pertemuan_3.Tugas_2;
+
+public class Mahasiswa {
+    public String nama;
+    public long nim;
+    public char kelamin;
+    public double ipk;
+
+    public Mahasiswa(String nama, long nim, char kelamin, double ipk){
+        this.nama = nama;
+        this.nim = nim;
+        this.kelamin = kelamin;
+        this.ipk = ipk;
+    }
+}
---
+++ src/Pertemuan_3/Tugas_2/MahasiswaMain.java
@@ -0,0 +1,31 @@
+package Pertemuan_3.Tugas_2;
+
+import java.util.Scanner;
+public class MahasiswaMain {
+    public static void main(String[] args) {
+        Scanner sc14 = new Scanner(System.in);
+        Mahasiswa[] mahasiswa = new Mahasiswa[4];
+
+        for (int i = 1; i < 4; i++){
+            System.out.println("Masukkan data mahasiswa ke-" + i);
+            System.out.print("Masukkan nama: ");
+            String nama = sc14.next();
+            System.out.print("Masukkan nim: ");
+            long nim = sc14.nextLong();
+            System.out.print("Masukkan jenis kelamin: ");
+            char kelamin = sc14.next().charAt(0);
+            System.out.print("Masukkan IPK: ");
+            double ipk = sc14.nextDouble();
+            mahasiswa[i] = new Mahasiswa(nama, nim, kelamin, ipk);
+        } 
+        
+        System.out.println("");
+        for (int i = 1; i < 4; i++){
+            System.out.println("Data mahasiswa ke-" + i);
+            System.out.println("Nama: " +  mahasiswa[i].nama);
+            System.out.println("NIM: " + mahasiswa[i].nim);
+            System.out.println("Jenis kelamin: " + mahasiswa[i].kelamin);
+            System.out.println("Nilai IPK: " + mahasiswa[i].ipk);
+        } sc14.close();
+    } 
+}
