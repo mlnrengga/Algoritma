caa43c786065b8fa0a11d9d9020c90f7385f81e9
mlnrengga <mlnrengga@gmail.com>
2024-05-19 20:44:29 +0700

    P12_Percobaan2

--- src/Pertemuan_12/DoubleLinkedList.java
+++ src/Pertemuan_12/DoubleLinkedList.java
@@ -84,4 +84,54 @@ public class DoubleLinkedList {
             System.out.println("Linked list kosong");
         }
     }
+
+    public void removeFirst()throws Exception{
+        if(isEmpty()){
+            throw new Exception ("Linked list masih kosong, tidak dapat dihapus");
+        } else if (size == 1){
+            removeLast();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    public void removeLast()throws Exception{
+        if (isEmpty()) {
+            throw new Exception ("Linked list masih kosong, tidak dapat dihapus");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;           
+        } 
+        current.next = null;
+        size--;
+    }
+
+    public void remove(int index) throws Exception{
+        if (isEmpty()){
+            throw new Exception ("Linked list masih kosong, tidak dapat dihapus");
+        } else if (index == 0){
+            removeFirst();
+        } else {
+            Node current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
+        } 
+    }
 }
--- src/Pertemuan_12/DoubleLinkedListMain.java
+++ src/Pertemuan_12/DoubleLinkedListMain.java
@@ -20,5 +20,23 @@ public class DoubleLinkedListMain {
         dll.print();
         System.out.println("size : " +dll.size());
         System.out.println("==============================");
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.print();
+        System.out.println("size : " +dll.size());
+        System.out.println("==============================");
+        dll.removeFirst();
+        dll.print();
+        System.out.println("size : " +dll.size());
+        System.out.println("==============================");
+        dll.removeLast();
+        dll.print();
+        System.out.println("size : " +dll.size());
+        System.out.println("==============================");
+        dll.remove(1);
+        dll.print();
+        System.out.println("size : " +dll.size());
     }
 }
