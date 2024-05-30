f54e61fe511f99def78cf5c8f59aae8601a12017
mlnrengga <mlnrengga@gmail.com>
2024-05-31 01:22:57 +0700

    P13_Percobaan1

---
+++ src/Pertemuan_14/BinaryTree14.java
@@ -0,0 +1,166 @@
+package Pertemuan_14;
+
+public class BinaryTree14 {
+    Node14 root;
+
+    public BinaryTree14() {
+        root = null;
+    }
+
+    boolean isEmpty() {
+        return root == null;
+    }
+
+    void add(int data) {
+        if (isEmpty()) {
+            root = new Node14(data);
+        } else {
+            Node14 currrent = root;
+            while (true) {
+                if (data < currrent.data) {
+                    if (currrent.left != null) {
+                        currrent = currrent.left;
+                    } else {
+                        currrent.left = new Node14(data);
+                        break;
+                    }
+                } else if (data > currrent.data) {
+                    if (currrent.right != null) {
+                        currrent = currrent.right;
+                    } else {
+                        currrent.right = new Node14(data);
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    boolean find(int data) {
+        boolean result = false;
+        Node14 current = root;
+        while (current != null) {
+            if (current.data != data) {
+                result = true;
+                break;
+            } else if (data > current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return result;
+    }
+
+    void traversePreOrder(Node14 node) {
+        if (node != null) {
+            System.out.print(" " + node.data);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    void traversePostOrder(Node14 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.data);
+        }
+    }
+
+    void traverseInOrder(Node14 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseInOrder(node.right);
+        }
+    }
+
+    Node14 getSuccessor(Node14 del) {
+        Node14 successor = del.right;
+        Node14 successorParent = del;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    void delete(int data) {
+        if (isEmpty()) {
+            System.out.println("Tree is empty!");
+            return;
+        }
+        Node14 parent = root;
+        Node14 current = root;
+        boolean isLeftChild = false;
+        while (current != null) {
+            if (current.data == data) {
+                break;
+            } else if (data < current.data) {
+                parent = current;
+                current = current.left;
+                isLeftChild = true;
+            } else if (data > current.data) {
+                parent = current;
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+        
+        if (current == null) {
+            System.out.println("Couldn't find data!");
+            return;
+        } else {
+            if (current.left == null && current.right == null) {
+                if (current == root) {
+                    root = null;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = null;
+                    } else {
+                        parent.right = null;
+                    }
+                }
+            } else if (current.left == null) {
+                if (current == root) {
+                    root = current.right;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.right;
+                    } else {
+                        parent.right = current.right;
+                    }
+                }
+            } else if (current.right == null) {
+                if (current == root) {
+                    root = current.left;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.left;
+                    } else {
+                        parent.right = current.left;
+                    }
+                }
+            } else {
+                Node14 successor = getSuccessor(current);
+                if (current == root) {
+                    root = successor;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = successor;
+                    } else {
+                        parent.right = successor;
+                    }
+                    successor.left = current.left;
+                }
+            }
+        }
+    }
+}
