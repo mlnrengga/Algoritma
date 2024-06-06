79a7f8f0856646b584f3449e23c7b5289ab98a9d
mlnrengga <mlnrengga@gmail.com>
2024-06-07 00:15:08 +0700

    P15_Percobaan2

--- src/Pertemuan_15/GraphMain14.java
+++ src/Pertemuan_15/GraphMain14.java
@@ -4,25 +4,25 @@ import java.util.Scanner;
 
 public class GraphMain14 {
     public static void main(String[] args) throws Exception {
-        // Scanner sc = new Scanner(System.in);
-        // Graph14 gedung = new Graph14(6);
-        // gedung.addEdge(0, 1, 50);
-        // gedung.addEdge(0, 2, 100);
-        // gedung.addEdge(1, 3, 70);
-        // gedung.addEdge(2, 3, 40);
-        // gedung.addEdge(3, 4, 60);
-        // gedung.addEdge(4, 5, 80);
-        // gedung.degree(0);
-        // gedung.printGraph();
+        Scanner sc = new Scanner(System.in);
+        Graph14 gedung = new Graph14(6);
+        gedung.addEdge(0, 1, 50);
+        gedung.addEdge(0, 2, 100);
+        gedung.addEdge(1, 3, 70);
+        gedung.addEdge(2, 3, 40);
+        gedung.addEdge(3, 4, 60);
+        gedung.addEdge(4, 5, 80);
+        gedung.degree(0);
+        gedung.printGraph();
 
-        // gedung.removeEdge(1, 3);
-        // gedung.printGraph();
+        gedung.removeEdge(1, 3);
+        gedung.printGraph();
 
-        // System.out.print("Masukkan gedung asal: ");
-        // int asal = sc.nextInt();
-        // System.out.print("Masukkan gedung tujuan: ");
-        // int tujuan = sc.nextInt();
-        // gedung.checkPath(asal, tujuan);
+        System.out.print("Masukkan gedung asal: ");
+        int asal = sc.nextInt();
+        System.out.print("Masukkan gedung tujuan: ");
+        int tujuan = sc.nextInt();
+        gedung.checkPath(asal, tujuan);
 
         GraphMatriks14 gdg = new GraphMatriks14(4);
         gdg.makeEdge(0, 1, 50);
