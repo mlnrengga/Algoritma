fbfb2a1b428af6803ffb77cee3501a4aace62bad
mlnrengga <mlnrengga@gmail.com>
2024-06-06 22:58:16 +0700

    P15_Percobaan1

---
+++ src/Pertemuan_15/DoubleLinkedList14.java
@@ -0,0 +1,173 @@
+package Pertemuan_15;
+
+public class DoubleLinkedList14 {
+    Node14 head;
+    int size;
+
+    public DoubleLinkedList14() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(int item, int jarak) {
+        if (isEmpty()) {
+            head = new Node14(null, item, jarak, null);
+        } else {
+            Node14 newNode14 = new Node14(null, item, jarak, head);
+            head.prev = newNode14;
+            head = newNode14;
+        }
+        size++;
+    }
+
+    public void addLast(int item, int jarak) {
+        if (isEmpty()) {
+            addFirst(item, jarak);
+        } else {
+            Node14 current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            Node14 newNode14 = new Node14(current, item, item, null);
+            current.next = newNode14;
+            size++;
+        }
+    }
+
+    public void add(int data, int index, int jarak) throws Exception {
+        if (isEmpty()) {
+            addFirst(data, jarak);
+        } else if (index < 0 || index > size) {
+            throw new Exception("Nilai indeks diluar batas");
+        } else {
+            Node14 current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.prev == null) {
+                Node14 newNode14 = new Node14(null, index, i, current);
+                current.prev = newNode14;
+                head = newNode14;
+            } else {
+                Node14 newNode14 = new Node14(current, index, i, current);
+                current.prev.next = newNode14;
+                current.prev = newNode14;
+            }
+        }
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node14 temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "\t");
+                temp = temp.next;
+            }
+            System.out.println("\nBerhasil diisi");
+        } else {
+            System.out.println("Linked list kosong");
+        }
+    }
+
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, tidak dapat dihapus");
+        } else if (size == 1) {
+            removeLast();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, tidak dapat dihapus");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node14 current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    public void remove(int index) {
+        Node14 current = head;
+        while (current != null) {
+            if (current.data == index) {
+                if (current.prev != null) {
+                    current.prev.next = current.next;
+                } else {
+                    head = current.next;
+                }
+                if (current.next != null) {
+                    current.next.prev = current.prev;
+                }
+                break;
+            }
+            current = current.next;
+        }
+    }
+
+    public int getFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list kosong");
+        }
+        return head.data;
+    }
+
+    public int getLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list kosong");
+        }
+        Node14 tmp = head;
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public int get(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks diluar batas.");
+        }
+        Node14 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public int getJarak(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        }
+        Node14 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.jarak;
+    }
+}
